e86bc89448bb982a58e3411d3ee4bc2d222e823e
StupidTomCat <1402437220@qq.com>
2021-02-14 16:59:34 +0800

    编写并测试JedisUtils

--- src/main/java/com/yyh/util/JedisUtils.java
+++ src/main/java/com/yyh/util/JedisUtils.java
@@ -4,21 +4,29 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.util.ResourceBundle;
+
 public class JedisUtils {
     private static JedisPool jp = null;
+    private static String host;
+    private static int port;
+    private static int maxTotal;
+    private static int maxIdle;
     static {
+        ResourceBundle rb = ResourceBundle.getBundle("redis");//redis配置文件名
+        host = rb.getString("redis.host");
+        port = Integer.parseInt(rb.getString("redis.port"));
+        maxTotal = Integer.parseInt(rb.getString("redis.maxTotal"));
+        maxIdle = Integer.parseInt(rb.getString("redis.maxIdle"));
         //JedisPool类是jedis提供的连接池，构造方法的参数有：GenericObjectPoolConfig poolConfig,String host,int port
         //poolConfig：连接池配置对象，host：redis地址，port：redis端口
         JedisPoolConfig jpc = new JedisPoolConfig();
-        jpc.setMaxTotal(30);//按照连接池思想，设置连接池最大连接数
-        jpc.setMaxIdle(10);//设置活动连接数
-        String host = "127.0.0.1";
-        int port = 6379;
+        jpc.setMaxTotal(maxTotal);//按照连接池思想，设置连接池最大连接数
+        jpc.setMaxIdle(maxIdle);//设置活动连接数
         jp = new JedisPool(jpc,host,port);
     }
 
     public static Jedis getJedis(){
-
         return jp.getResource();
     }
 }
--- src/test/java/com/yyh/JedisTest.java
+++ src/test/java/com/yyh/JedisTest.java
@@ -1,15 +1,24 @@
 package com.yyh;
 
+import com.yyh.util.JedisUtils;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
 
 public class JedisTest {
     @Test
     public void helloJedis(){
+//        //连接redis
+//        Jedis jedis = new Jedis("127.0.0.1", 6379);
+//        //操作redis
+//        jedis.set("message","helloJedis");
+//        String message = jedis.get("message");
+//        System.out.println(message);
+//        //关闭连接
+//        jedis.close();
         //连接redis
-        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        Jedis jedis = JedisUtils.getJedis();
         //操作redis
-        jedis.set("message","helloJedis");
+        jedis.set("message","hiJedis");
         String message = jedis.get("message");
         System.out.println(message);
         //关闭连接
